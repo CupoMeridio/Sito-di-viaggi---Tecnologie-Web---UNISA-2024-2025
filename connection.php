d8355c182a0dd39c551b028ada6a0eea25d9be89
Mattia <mattiasanzari2003@gmail.com>
2025-02-22 23:31:15 +0100

    Messo valori giusti per la connessione. Forse la parola FINE sarà MIAAAA

--- connection.php
+++ connection.php
@@ -1,15 +1,23 @@
 <?php
+/* Database Online 
 $host='database-1.czikiq82wrwk.eu-west-2.rds.amazonaws.com';
-//$host='localhost';
 $port = '5432';
 $db= 'tw_prova';
-//$db='tw_gruppo11';
-//$user='www';
-//$pass='tw2024';
 $user= 'postgres';
-$pass= 'Farinotta01_';
+$pass= 'Farinotta01_'; */
+
+/*  Database Offline */
+$host='postgres';
+$port = '5432';
+$db='gruppo11';
+$user='www';
+$pass='tw2024';
+
+//echo  "Prima";
 
 $connection_string = "host=$host port=$port dbname=$db user=$user password=$pass";
 $db= pg_connect( $connection_string) or die('Impossibile connetersi al database:'.pg_last_error());
+
+//echo  "Dopo";
 
 ?>
