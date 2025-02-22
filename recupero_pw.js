3896569a9185e9233fd4eb16359e792f3c377363
Rosa <rosapassaro28@gmail.com>
2025-02-22 15:04:37 +0100

    js

---
+++ recupero_pw.js
@@ -0,0 +1,84 @@
+const videos = [
+    'video/registrazione/Video1.mp4',
+    'video/registrazione/Video2.mp4',
+    'video/registrazione/Video3.mp4',
+    'video/registrazione/Video4.mp4',
+  ];
+
+  // Seleziona un video casuale
+  const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+  // Imposta il video come sorgente
+  const videoElement = document.getElementById('background-video');
+  videoElement.innerHTML = `<source src="${randomVideo}" type="video/mp4">`;
+
+  // Aggiunge un messaggio se il browser non supporta il video
+  videoElement.innerHTML += 'Errore nella riproduzione del video.';
+
+
+//------------ xona email recuperp------------
+const emailProviders = [
+    "@gmail.com",
+    "@outlook.com",
+    "@yahoo.com",
+    "@icloud.com",
+    "@protonmail.com",
+    "@zoho.com",
+    "@gmx.com",
+    "@aol.com",
+    "@mail.com",
+    "@libero.it",
+    "@tiscali.it",
+    "@fastwebnet.it",
+    "@email.it",
+    "@aruba.it",
+    "@kataweb.it",
+    "@studenti.unisa.it" ,
+    "@hotmail.it",
+    "@hotmail.com"
+  ];
+
+function patternEmail(email){
+    let pattern = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/; 
+    if (!pattern.test(email)){   
+        //alert("sono nell'if");     
+        return false;
+    }else{
+        let split=email.split('@');
+        let dominio = '@'+split[1];
+        return emailProviders.includes(dominio);
+    }
+}
+
+//CONTROLLO EMAIL PATTERN 
+document.getElementById("email").addEventListener("input", function(){
+    let email =document.getElementById("email").value;
+    let email_style =document.getElementById("email");
+    if(!patternEmail(email))
+       email_style.style.outline="solid red 3px";
+    else{
+        email_style.style.outline="solid green 3px";
+    }
+
+});
+
+//----------PATTERN PASSWORD
+function patternPassword(password){
+    let pattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
+    return pattern.test(password);
+}
+
+document.getElementById("Cambia_password").addEventListener("input", function(){
+    alert("sono qui");
+    let password=document.getElementById("Cambia_password").value;
+    let password_style=document.getElementById("Cambia_password");
+    let password_error=document.getElementById("error");
+   
+    if(patternPassword(password)){
+        password_style.style.outline="solid 3px green";
+        password_error.textContent="";
+    }else{
+        password_style.style.outline="solid 3px red";
+        password_error.textContent="La password non rispetta i requisiti";
+    }
+});
--- recupero_pw.php
+++ recupero_pw.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 //session_unset();
 session_start();
 include('controlloGenerale.php');
 include('connection.php');
 
-$password="";
-$email="";
+$password = "";
+$email = "";
 //echo $_POST['email'];
-if (isset($_POST['email'])) 
+if (isset($_POST['email']))
     $email = $_POST['email'];
 //echo $email;
-if($email != ""){
+if ($email != "") {
     //echo $email;
-    if(!controlloPatternEmail($email)){   
-        $_SESSION['pw_problem'] = "Errore nella forma della email"; 
-    }else{ 
+    if (!controlloPatternEmail($email)) {
+        $_SESSION['pw_problem'] = "Errore nella forma della email";
+    } else {
         $query_no_injection = "SELECT nome, cognome FROM utente WHERE email=$1";
         $result = pg_prepare($db, "select_email", $query_no_injection);
         $values = array($email);
@@ -30,108 +30,113 @@ if($email != ""){
             $_SESSION['email'] = $email;
 
             $body = "Ecco il codice per resettare la password: " . $_SESSION['codice'];
-            $subject= 'Recupero password';
+            $subject = 'Recupero password';
             include('invia_email.php');
-        }else{
+        } else {
             $_SESSION['pw_problem'] = "<div><p>Non hai ancora un account  <a href='registrazione.php#form-registrazione'>Registrati</a></p></div> ";
         }
     }
- 
 }
 
-if(isset($_POST['Cambia_password']))
-    $password= $_POST['Cambia_password'];
+if (isset($_POST['Cambia_password']))
+    $password = $_POST['Cambia_password'];
 
-if($password != "" ){
-   if( controlloPatternPassword($password)){
- if (isset($_POST['codice_cambia_password']) && isset($_POST['Cambia_password'])) {
-    if ($_POST['codice_cambia_password'] == $_SESSION['codice']) {
-        $query_no_injection = "UPDATE utente SET password=$1 WHERE email=$2";
-        $result = pg_prepare($db, "cambio_pw", $query_no_injection);
-        $hash = password_hash($password, PASSWORD_DEFAULT);
-        $values = array($hash, $_SESSION['email']);
-        $result = pg_execute($db, "cambio_pw", $values);
+if ($password != "") {
+    if (controlloPatternPassword($password)) {
+        if (isset($_POST['codice_cambia_password']) && isset($_POST['Cambia_password'])) {
+            if ($_POST['codice_cambia_password'] == $_SESSION['codice']) {
+                $query_no_injection = "UPDATE utente SET password=$1 WHERE email=$2";
+                $result = pg_prepare($db, "cambio_pw", $query_no_injection);
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $values = array($hash, $_SESSION['email']);
+                $result = pg_execute($db, "cambio_pw", $values);
 
-        if ($result) {
-            echo "Password cambiata";
-        } else {
-            echo "Problema con la procedura - password non cambiata";
-        }
-        include('logout.php');
-    } else {
-        echo "Il codice non è corretto " . $_POST['codice_cambia_password'];
-        session_unset();
-        session_destroy();
-        if (isset($_SERVER['HTTP_COOKIE'])) {
-            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-            foreach ($cookies as $cookie) {
-                $parts = explode('=', $cookie);
-                $name = trim($parts[0]);
-                setcookie($name, '', time() - 3600, "/");
+                if ($result) {
+                    echo "Password cambiata";
+                } else {
+                    echo "Problema con la procedura - password non cambiata";
+                }
+                include('logout.php');
+            } else {
+                echo "Il codice non è corretto " . $_POST['codice_cambia_password'];
+                session_unset();
+                session_destroy();
+                if (isset($_SERVER['HTTP_COOKIE'])) {
+                    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+                    foreach ($cookies as $cookie) {
+                        $parts = explode('=', $cookie);
+                        $name = trim($parts[0]);
+                        setcookie($name, '', time() - 3600, "/");
+                    }
+                }
             }
         }
-    }       
+    } else {
+        $_SESSION['pw_problem_cambio'] = " La password deve rispettare il pattern ";
     }
-}else{
-    $_SESSION['pw_problem_cambio']=" La password deve rispettare il pattern " ;
-   }
 }
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Recupero password</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="registrazioneStyle.css">                                               <!-- Collega il file CSS esterno per definire gli stili visivi della pagina -->
+    <link rel="stylesheet" href="registrazioneStyle.css"> <!-- Collega il file CSS esterno per definire gli stili visivi della pagina -->
     <?php include("commons/setIcon.html"); ?>
+    <script src="recupero_pw.js" type="text/javascript" defer="true"></script>
+    <style>
+        #error {
+            color: crimson;
+        }
+
+        #hint {
+            color: grey;
+        }
+    </style>
 </head>
+
 <body>
-<video id="background-video" autoplay muted loop></video>    
-<div id="main-container" class="regcontainer" style="display: block;"> 
-<?php  
-if (!isset($_SESSION['codice'])) { //echo $_SESSION['codice'];?>
-    <!-- Contenitore principale per il modulo di registrazione -->
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="email">Inserisci la tua email:</label>
-    <input type="text" id="email" name="email">
-    <input type="submit" value="Invio" style="width: 100%; margin-top:10px;";>
-</form>
-<?php if(isset($_SESSION['pw_problem'])){ echo $_SESSION['pw_problem'];} ?>
-<?php } else { ?>
+    <video id="background-video" autoplay muted loop></video>
+    <div id="main-container" class="regcontainer" style="display: block;">
+        <?php
+        if (!isset($_SESSION['codice'])) { //echo $_SESSION['codice'];
+        ?>
+            <!-- Contenitore principale per il modulo di registrazione -->
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <label for="email">E-mail inserita:</label>
+                <input type="text" id="email" name="email" value="<?php $email ?>">
+                <input type="submit" value="Invio" style="width: 100%; margin-top:10px;" ;>
+            </form>
+            <?php if (isset($_SESSION['pw_problem'])) {
+                echo $_SESSION['pw_problem'];
+            } ?>
+        <?php } else { ?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="Cambia_password">Scrivi la nuova password:</label>
-    <input type="text" id="Cambia_password" name="Cambia_password" required>
-    <label for="codice_cambia_password">Scrivi il codice:</label>
-    <input name="codice_cambia_password" id="codice_cambia_password" type="text" pattern="\d{6}" required>
-    <input type="submit" value="Invio" style="width: 100%; margin-top:10px;">
-</form>
-<?php if(isset($_SESSION['pw_problem_cambio'])){ echo $_SESSION['pw_problem_cambio'];} ?>
-</div>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <div class="container">
+                    <label for="Cambia_password">Scrivi la nuova password:</label>
+                    <input type="text" id="Cambia_password" name="Cambia_password" required>
+                    <div id="hint" style="margin: bottom 10px; font-size: 13px;">La lunghezza della password deve essere di almeno 8 caratteri. <br>La password deve contenere almeno una lettera maiuscola e almeno un carattere speciale.</div>
+                    <div id="error"></div>
+                </div>
+                <p class="container">
+                    <label for="codice_cambia_password">Scrivi il codice:</label>
+                    <input name="codice_cambia_password" id="codice_cambia_password" type="text" pattern="\d{6}" required>
+                </p>
+                <input type="submit" value="Invio" style="width: 100%; margin-top:10px;">
+            </form>
+            <?php if (isset($_SESSION['pw_problem_cambio'])) {
+                echo $_SESSION['pw_problem_cambio'];
+            } ?>
+    </div>
 <?php
- } ?>
+        } ?>
+
+
 </body>
-<script>
-// Array dei percorsi dei video
-    const videos = [
-      'video/registrazione/Video1.mp4',
-      'video/registrazione/Video2.mp4',
-      'video/registrazione/Video3.mp4',
-      'video/registrazione/Video4.mp4',
-    ];
 
-    // Seleziona un video casuale
-    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
-
-    // Imposta il video come sorgente
-    const videoElement = document.getElementById('background-video');
-    videoElement.innerHTML = `<source src="${randomVideo}" type="video/mp4">`;
-
-    // Aggiunge un messaggio se il browser non supporta il video
-    videoElement.innerHTML += 'Errore nella riproduzione del video.';
-    
-</script>
 
 </html>
 
