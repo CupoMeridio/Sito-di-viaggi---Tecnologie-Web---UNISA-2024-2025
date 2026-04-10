1b9fccb7339a9078c6c12752fff608473d15581d
CupoMeridio <cupo-meridio@live.it>
2026-04-10 23:46:00 +0200

    refactor(navbar): extract navbar into reusable component
    Move navbar HTML from index.php to a dedicated component file and update CSS to improve layout management. This centralizes navbar logic and styling, making the codebase more maintainable and consistent across pages.

--- components/navbar.php
+++ components/navbar.php
@@ -3,10 +3,12 @@
     <a class="navButton" id="homeButton" href="index.php">Home</a>
     <a class="navButton" id="aboutButton"href="index.php#about-section">About</a>
     <a class="navButton" id="contactButton" href="index.php#contact-section">Contact</a>
-    
+
     <?php if(!isset($email)){?>
-    <a class="navButton" id="registrazioneButton" href="autenticazione.php">Registrati</a>
-    <a class="navButton" id="accessoButton" href="autenticazione.php?login">Accedi</a>
+    <div id="navAuth">
+        <a class="navButton" id="registrazioneButton" href="autenticazione.php">Registrati</a>
+        <a class="navButton" id="accessoButton" href="autenticazione.php?login">Accedi</a>
+    </div>
     <?php }?>
     
     <?php if(isset($email)){?>
--- index.php
+++ index.php
@@ -23,26 +23,7 @@ include 'api/recupera_dati_utente.php';
 
 <body>
 
-    <nav id="navbar">
-    <a href="index.php"><img src="img/common/logo.png"></a>
-    <a class="navButton" id="aboutButton"href="index.php#about-section">About</a>
-    <a class="navButton" id="contactButton" href="index.php#contact-section">Contact</a>
-    
-    <?php if(!isset($email)){?>
-    <a class="navButton" id="registrazioneButton" href="autenticazione.php">Registrati</a>
-    <a class="navButton" id="accessoButton" href="autenticazione.php?login">Accedi</a>
-    <?php }?>
-    
-    <?php if(isset($email)){?>
-    <!-- Sezione profilo utente -->
-    <div id="userProfile">
-        <span id="welcomeMessage"><?php  echo "Ciao, $username";?> </span>
-         <?php echo '<img id="profilePic" src="'.$img.'">'; ?>
-        
-    </div>
-    <?php }?>
-    
-  </nav>
+    <?php include 'components/navbar.php'; ?>
   
     <?php include 'components/dashboard.html'; ?>
     
